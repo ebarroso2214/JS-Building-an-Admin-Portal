a88829ebf5f1c70581ddca3676ae9350802ab3f1
ebarroso2214 <ebarroso2214@gmail.com>
2023-02-10 19:39:24 -0500

    finished assignment

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,47 @@
 
 // Your Code Here
+
+
+async function listBooks (){
+    let response = await fetch ('http://localhost:3001/listBooks', {
+    
+
+
+    });
+
+    let books = await response.json();
+    books.forEach(addBook)
+}
+
+
+function addBook(book){
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch ('http://localhost:3001/updateBook', {
+            method:'PATCH',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                id:book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+  }
+
+  listBooks();
